d4637a8c9c4bdd6d356ecc173caaf6c603496417
Phil de Joux <philderbeast@gmail.com>
2021-11-20 13:19:34 -0500

    Use the comp.compName in the breadcrumb.

--- src/lib/Breadcrumb.svelte
+++ src/lib/Breadcrumb.svelte
@@ -1,10 +1,12 @@
 <script context="module">
+	import { comp } from '../store.js';
+
 	export const prerender = true;
 </script>
 
 <nav class="breadcrumb">
 	<ul>
 		<li><a href="http://flaretiming.com">Flare Timing</a></li>
-		<li class="is-active"><a href="#">Dalby Big Air HG 2017 open class</a></li>
+		<li class="is-active"><a href="#">{$comp.compName}</a></li>
 	</ul>
 </nav>
