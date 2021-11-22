8e62ee5d9bdd43785701c14167c91253acadd086
Phil de Joux <philderbeast@gmail.com>
2021-11-22 11:05:00 -0500

    Add links to flare timing and svelte, format document.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,10 @@
 		<div class="tile is-parent">
 			<article class="tile is-child notification is-light">
 				<p class="title">Variable Geometry</p>
-				<p class="subtitle">Comp scored with Flare Timing and presented with Svelte.</p>
+				<p class="subtitle">
+					Comp scored with <a href="https://flaretiming.com">Flare Timing</a> and presented with
+					<a href="https://svelte.dev/">Svelte</a>
+				</p>
 			</article>
 		</div>
 	</div>
@@ -24,12 +27,12 @@
 						<ul>
 							<li>
 								Italian Open
-								<a href="/comp" on:click={() => $compPrefix = "2020-italy-open"}>2020</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2020-italy-open')}>2020</a>
 							</li>
 							<li>
 								Dalmatian
-								<a href="/comp" on:click={() => $compPrefix = "2019-dalmatian"}>2019</a>
-								<a href="/comp" on:click={() => $compPrefix = "2018-dalmatian"}>2018</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2019-dalmatian')}>2019</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2018-dalmatian')}>2018</a>
 							</li>
 							<ul>
 								<p />
@@ -41,10 +44,14 @@
 						<p />
 						<ul>
 							<li>
-								<a href="/comp" on:click={() => $compPrefix = "1976-never-land"}>1976 Never Land</a>
+								<a href="/comp" on:click={() => ($compPrefix = '1976-never-land')}
+									>1976 Never Land</a
+								>
 							</li>
 							<li>
-								<a href="/comp" on:click={() => $compPrefix = "1989-lift-lines"}>1989 Lift Lines</a>
+								<a href="/comp" on:click={() => ($compPrefix = '1989-lift-lines')}
+									>1989 Lift Lines</a
+								>
 							</li>
 							<ul>
 								<p />
@@ -63,42 +70,44 @@
 						<ul>
 							<li>
 								Forbes Flatlands
-								<a href="/comp" on:click={() => $compPrefix = "2018-forbes"}>2018</a>
-								<a href="/comp" on:click={() => $compPrefix = "2017-forbes"}>2017</a>
-								<a href="/comp" on:click={() => $compPrefix = "2016-forbes"}>2016</a>
-								<a href="/comp" on:click={() => $compPrefix = "2015-forbes"}>2015</a>
-								<a href="/comp" on:click={() => $compPrefix = "2014-forbes"}>2014</a>
-								<a href="/comp" on:click={() => $compPrefix = "2012-forbes"}>2012</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2018-forbes')}>2018</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2017-forbes')}>2017</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2016-forbes')}>2016</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2015-forbes')}>2015</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2014-forbes')}>2014</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2012-forbes')}>2012</a>
 							</li>
 							<li>
-								Dalby Big Air <a href="/comp" on:click={() => $compPrefix = "2017-dalby"}>2017</a>
+								Dalby Big Air <a href="/comp" on:click={() => ($compPrefix = '2017-dalby')}>2017</a>
 							</li>
 						</ul>
 						<h5>Europe</h5>
 						<ul>
 							<li>
 								Meduno
-								<a href="/comp" on:click={() => $compPrefix = "2020-meduno"}>2020</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2020-meduno')}>2020</a>
 							</li>
 							<li>
 								Tolmezzo
-								<a href="/comp" on:click={() => $compPrefix = "2019-italy"}>2019</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2019-italy')}>2019</a>
 							</li>
 						</ul>
 						<h5>Americas</h5>
 						<ul>
 							<li>
 								Green Swamp Klassic 2016
-								<a href="/comp" on:click={() => $compPrefix = "2016-greenswamp"}>Topless</a>
-								<a href="/comp" on:click={() => $compPrefix = "2016-greenswamp-sport"}>Kingposted</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2016-greenswamp')}>Topless</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2016-greenswamp-sport')}
+									>Kingposted</a
+								>
 							</li>
 							<li>
 								Big Spring
-								<a href="/comp" on:click={() => $compPrefix = "2016-big-spring"}>2016</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2016-big-spring')}>2016</a>
 							</li>
 							<li>
 								QuestAir Open
-								<a href="/comp" on:click={() => $compPrefix = "2016-quest"}>2016</a>
+								<a href="/comp" on:click={() => ($compPrefix = '2016-quest')}>2016</a>
 							</li>
 						</ul>
 					</div>
